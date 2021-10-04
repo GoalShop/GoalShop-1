03f304c742cc91d66887a734fa4db63fde4c3c86
ninhphuoc121 <91859987+GoalShop@users.noreply.github.com>
2021-10-04 14:40:05 +0700

    Initial commit

---
+++ src/main/resources/static/assets/admin/admin-app.js
@@ -0,0 +1,32 @@
+app = angular.module("admin-app", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/product", {
+            templateUrl: "/assets/admin/product/index.html",
+            controller: "product-ctrl"
+        })
+        .when("/authorize", {
+            templateUrl: "/assets/admin/authority/index.html",
+            controller: "authority-ctrl"
+        })
+        .when("/category", {
+            templateUrl: "/assets/admin/category/index.html",
+            controller: "category-ctrl"
+        })
+         .when("/account", {
+            templateUrl: "/assets/admin/account/index.html",
+            controller: "account-ctrl"
+        })
+        .when("/char", {
+            templateUrl: "/assets/admin/char/index.html",
+
+        })
+        .when("/char2", {
+            templateUrl: "/assets/admin/char2/index.html",
+
+        })
+        .otherwise({
+            template: "<h1 class='text-center'>Ninh van phuoc</h1>"
+        });
+})
